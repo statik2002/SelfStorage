54e46a7153e18ce574ec90a60a25250fe68c2523
statik2002 <statik2002@mail.ru>
2023-11-14 16:11:50 +0800

    change templates

--- SelfStorage/main/urls.py
+++ SelfStorage/main/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 
 from main import views
 
+app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('boxes/', views.boxes_view, name='boxes'),
+    path('faq/', views.faq_view, name='faq')
 ]
