ae44ca5c57a9cf539769f7d311f7e094085042c1
Anton Ozerov <lamerork@gmail.com>
2023-11-15 12:36:38 +0300

    Добавил модели

--- SelfStorage/main/models.py
+++ SelfStorage/main/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomerManager(BaseUserManager):
@@ -56,3 +57,171 @@ class Customer(AbstractUser):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+
+class Storage(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Склад'
+    )
+    description = models.TextField(
+        verbose_name='Описание'
+    )
+    address = models.CharField(
+        max_length=200,
+        verbose_name='Адрес склада'
+    )
+    phone = PhoneNumberField(
+        verbose_name='Телефон',
+        region='RU'
+    )
+    email = models.EmailField(
+        max_length=256,
+        unique=True,
+        verbose_name='Email склада'
+    )
+    map = models.CharField(
+        max_length=200,
+        verbose_name='Карта'
+    )
+    temperature = models.DecimalField(
+        max_digits=2,
+        decimal_places=1,
+        verbose_name='Температура на складе'
+    )
+    heigth = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        verbose_name='Высота потолка'
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
+
+
+class Box(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Бокс'
+    )
+    storage = models.ForeignKey(
+        Storage,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Склад',
+        related_name='box'
+    )
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=1,
+        verbose_name='Стоимость'
+    )
+    length = models.IntegerField(
+        verbose_name='Длина'
+    )
+    weigth = models.IntegerField(
+        verbose_name='Ширина'
+    )
+    heigth = models.IntegerField(
+        verbose_name='Высота'
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Бокс'
+        verbose_name_plural = 'Боксы'
+
+
+class Status(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='Статус'
+    )
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Rent(models.Model):
+    renter = models.ForeignKey(
+        Customer,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Пользователь',
+        related_name='user_rent'
+    )
+    box = models.ForeignKey(
+        Box,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Бокс',
+        related_name='box_rent'
+    )
+    start_date = models.DateField(
+        verbose_name='Дата начала аренды'
+    )
+    end_date = models.DateField(
+        verbose_name='Дата окончания аренды'
+    )
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        verbose_name='Статус',
+        related_name='rent'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.renter} - {self.box}'
+
+    class Meta:
+        verbose_name = 'Бокс'
+        verbose_name_plural = 'Боксы'
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    storage = models.ForeignKey(
+        Storage,
+        on_delete=models.CASCADE,
+        verbose_name='Склад',
+        related_name='image'
+    )
+
+    def __str__(self) -> str:
+        return self.storage
+
+    class Meta:
+        verbose_name = 'Картинка'
+        verbose_name_plural = 'Картинки'
+
+
+class UtmMark(models.Model):
+    url = models.CharField(
+        max_length=200,
+        verbose_name='Ссылка'
+    )
+    count = models.IntegerField(
+        verbose_name='Количество переходов'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.count} - {self.url}'
+
+    class Meta:
+        verbose_name = 'Переход'
+        verbose_name_plural = 'Переходы'
