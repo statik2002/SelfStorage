d63678848a1d81e50dcc8a5bbdef5ee976b7c88b
Anton Ozerov <lamerork@gmail.com>
2023-11-15 13:38:26 +0300

    Добавил админку к моделям

--- SelfStorage/main/admin.py
+++ SelfStorage/main/admin.py
@@ -1,8 +1,101 @@
 from django.contrib import admin
 
 from main.models import Customer
-
+from main.models import Storage, Box, Status, Rent, Image, UtmMark
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number', 'date_joined')
+
+
+@admin.register(Storage)
+class StorageAdmin(admin.ModelAdmin):
+    search_fields = [
+        'title',
+        'phone',
+        'email',
+        'address',
+    ]
+    list_display = [
+        'title',
+        'phone',
+        'email',
+        'address',
+    ]
+
+
+@admin.register(Box)
+class BoxAdmin(admin.ModelAdmin):
+    search_fields = [
+        'title',
+        'storage',
+        'price',
+    ]
+    list_display = [
+        'title',
+        'storage',
+        'price',
+        'length',
+        'weigth',
+        'heigth',
+    ]
+    raw_id_fields = [
+        'storage',
+    ]
+
+
+@admin.register(Rent)
+class RentAdmin(admin.ModelAdmin):
+    search_fields = [
+        'renter',
+        'box',
+        'status',
+    ]
+    list_display = [
+        'renter',
+        'box',
+        'status',
+        'start_date',
+        'end_date',
+    ]
+    raw_id_fields = [
+        'renter',
+        'box',
+        'status',
+    ]
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    search_fields = [
+        'title',
+    ]
+    list_display = [
+        'title',
+    ]
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    search_fields = [
+        'storage',
+        'image',
+    ]
+    list_display = [
+        'storage',
+        'image',
+    ]
+    raw_id_fields = [
+        'storage',
+    ]
+
+
+@admin.register(UtmMark)
+class UtmMarkAdmin(admin.ModelAdmin):
+    search_fields = [
+        'url',
+    ]
+    list_display = [
+        'url',
+        'count',
+    ]
