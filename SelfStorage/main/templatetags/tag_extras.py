0d30e6c35daf32f05c3a39110ac6024b0b1c2071
statik2002 <statik2002@mail.ru>
2023-11-17 16:22:59 +0800

    fix calc square params

--- SelfStorage/main/templatetags/tag_extras.py
+++ SelfStorage/main/templatetags/tag_extras.py
@@ -5,9 +5,9 @@ register = template.Library()
 
 @register.filter(name='to_meters')
 def to_meters(value: int):
-    return value/1000
+    return value/100
 
 
 @register.filter(name="to_sqm")
 def to_square_meters(width: int, length: int):
-    return width*length/1000
+    return width*length/100
