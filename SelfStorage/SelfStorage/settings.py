ae075bfb8294876b0a8d5843a6eb51652d6ef75e
Anton Ozerov <lamerork@gmail.com>
2023-11-18 12:38:00 +0300

    Добавил STATIC_ROOT

--- SelfStorage/SelfStorage/settings.py
+++ SelfStorage/SelfStorage/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
@@ -148,3 +151,4 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERYD_CHDIR='/opt/SelfStorage/SelfStorage'
+
